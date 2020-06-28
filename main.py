759b33b117a58731338141a791498ae79adaafe6
AltruisticCandy8 <altr899@mail.ru>
2020-06-28 20:28:18 +0300

    Decorator

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello, Users!")
+	# Наш декоратор
+def decorator(function):
+		# Wrapped - функция обертка
+	def wraped():
+		print("1")
+			# Изменяемый объект
+		function()
+
+	return wraped
+
+	'''
+		Decorator является оберткой для функции someFunction
+		С помошью обертки, мы можем изменять данные, и
+		функционал объектов
+									'''
+@decorator
+def someFunction():
+
+	print("2")
+
+	'''
+		В случае, если функия будет изменена Decorator.
+		Полученный результат должен быть раверн 1 2
+									'''
+
+someFunction()
